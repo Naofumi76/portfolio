b46d71963ff803e19f3cb69f8f065fe5253e1772
nsannier <Naofumi76@users.noreply.github.com>
2025-03-04 13:36:24 +0100

    More school info + better contact page

--- src/js/contact.js
+++ src/js/contact.js
@@ -3,11 +3,49 @@ export default function () {
 	<div class="contact">
 		<h2>Contact</h2>
 		<p>Feel free to reach out to me!</p>
-		<div class="contact-info">
-			
-			/* Here will be links to github etc */
-			
-			
+		<div class="social-info">
+			<div class="info"> 			
+				<a href="https://github.com/Naofumi76" class="social-link" target="_blank">
+					<img src="img/github-icon.svg" alt="GitHub">
+					<span> - Github</span>
+				</a>
+			</div>
+
+
+			<div class="info">
+				<a href="https://linkedin.com/" class="social-link" target="_blank">
+					<img src="img/linkedin-icon.svg" alt="LinkedIn">
+					<span> - LinkedIn</span>
+				</a>
+			</div>
+
+			<div class="info">
+				<a href="mailto:sanniernathan@gmail.com" class="social-link">
+					<img src="img/gmail-icon.svg" alt="Email">
+					<span> - Email</span>
+				</a>
+			</div>
+
+			<div class="info">
+				<a href="https://discord.com" class="social-link" target="_blank">
+					<img src="img/discord-icon.svg" alt="Discord">
+					<span> - Discord</span>
+				</a>
+			</div>
+
+			<div class="info">
+				<a href="files/Nathan_Sannier_CV.pdf" class="social-link" download="Nathan_Sannier_CV.pdf">
+					<img src="img/cv-icon.png" alt="CV">
+					<span> - My CV</span>
+				</a>
+			</div>
+
+			<div class="info">
+			    <a href="tel:+33650387459" class="social-link">
+					<img src="img/phone-icon.svg" alt="Phone">
+					<span> - +33 6 50 38 74 59</span>
+				</a>
+			</div>
 		</div>
 	</div>
 	 `
--- src/js/school.js
+++ src/js/school.js
@@ -3,9 +3,26 @@ export default function () {
 		<div class="school">
 			<h1>Zone01</h1>
 			<p>
-			Zone01 is a private school from the Campus St Marc located in Rouen that offers a course to be a full-stack developer.
-			The program consist of multiple "piscines" and exercises. Each piscine is supposed to let us understand and learn the basics of a language
- 			(such as Golang, Javascript, and Rust), while exercises consists of projects that are real applications of the language we learned.
+			Zone01 is a private school at the Campus St Marc located in Rouen that offers a course to become a full-stack developer.
+			The program consists of multiple "piscines" and exercises. Each piscine is designed to help us understand and learn the basics of a language
+ 			(such as Golang, JavaScript, and Rust), while the exercises consist of projects that are real-world applications of the language we learned.
+			</p>
+
+			<p>
+			The program at this school is peer-to-peer, meaning that there are no professors to teach us. Instead, we have to learn independently as students.
+			There are projects for both individual students and groups of two to eight students.
+			</p>
+
+			<p> 
+			Projects are both interesting and important because they are real exercises that introduce us to work. Projects include creating a forum, making an ant farm simulator, a social network, etc.
+			</p>
+
+			<p> 
+			At the end of the course, I will obtain an RNCP Level 6 title, officially recognized by the French Government. This title represents 90 European Credits, equivalent to 3 years of education. 
+			</p>
+
+			<p>
+			If you want to know more about it, <a href="https://zone01rouennormandie.org/" class="link-highlight" target="_blank">click here</a> to check out Zone01's website.
 			</p>
 		</div>
 	`
