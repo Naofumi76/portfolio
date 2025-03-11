4c0723d42a49961e314ab47e9733e403b8df2298
nsannier <Naofumi76@users.noreply.github.com>
2025-03-11 12:26:34 +0100

    Tried adding a scroll for skills

--- src/js/skills.js
+++ src/js/skills.js
@@ -1,26 +1,30 @@
 export default function () {
-	return `
-	<div class="skills">
-		<h2>Here you can see what I'm most familiar with for development purposes.</h2>
-		<br>
-		<h3>Back-end</h3>
-		<ul>
-		    <li>Python</li>
-			<li>Golang</li>
-			<li>SQL</li>
-			<li>SQLite3</li>
-			<li>GraphQL</li>
-			<li>Rust</li>
-		</ul>
+    return `
+    <div class="skills">
+        <h2>Here you can see what I'm most familiar with for development purposes.</h2>
+        <br>
+        <h3>Back-end</h3>
+        <div class="scroll-container">
+            <ul class="scrolling-list">
+                <li>Python</li>
+                <li>Golang</li>
+                <li>SQL</li>
+                <li>SQLite3</li>
+                <li>GraphQL</li>
+                <li>Rust</li>
+            </ul>
+        </div>
 
-		<h3>Front-end</h3>
-		<ul>
-			<li>HTML</li>
-			<li>CSS</li>
-			<li>JavaScript</li>
-			<li>React</li>
-			<li>Node.js</li>
-		</ul>
-	</div>
-	`
+        <h3>Front-end</h3>
+        <div class="scroll-container">
+            <ul class="scrolling-list">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node.js</li>
+            </ul>
+        </div>
+    </div>
+    `;
 }
